799a9b6c615257c9c5c826fb360982efdd7818de
Mufeed Ali <fushinari@protonmail.com>
2021-05-29 15:40:37 +0530

    Fix wallis_pi

--- wallis.c
+++ wallis.c
@@ -5,6 +5,16 @@
 
 float wallis_pi(int);
 
+float wallis_pi(int count) {
+  float pi = 1.0;
+  for (int i = 1; i < count; i++)
+  {
+    float num = 4.0 * i * i;
+    pi *= num / (num - 1);
+  }
+  return pi * 2;
+}
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
